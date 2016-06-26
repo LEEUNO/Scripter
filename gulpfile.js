3e1d184d7dc988a6298d680c91758e3bfba29ea6
LEEUNO <unolee48@gmail.com>
2016-06-26 15:40:23 +0900

    test1

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+
+var watch = require('gulp-watch');
+var html2js = require('gulp-html2js');
+
+
+var paths = {
+  sass: ['./templates/**/*.scss', './www/templates/**/*.scss', './scss/**/*.scss'],
+  ionicSass: ['./scss/*.scss']
+  //templates: ['./templates/**/*.html']
+};
+
+gulp.task('default', ['watch']);
+
+
+gulp.task('sass-ionic', function (done) {
+  gulp.src(paths.ionicSass)
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .on('error', sass.logError)
+    .pipe(concat('ionic.app.min.css'))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+gulp.task('sass', function (done) {
+  gulp.src(paths.sass)
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .on('error', sass.logError)
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+
+gulp.task('watch', ['sass', 'sass-ionic'], function () {
+  watch(paths.sass, function () {
+    gulp.start('sass');
+  });
+  watch(paths.ionicSass, function () {
+    gulp.start('sass-ionic');
+  });
+  //watch(paths.templates, function () {
+  //  gulp.start('templates');
+  //})
+});
+
+gulp.task('install', ['git-check'], function () {
+  return bower.commands.install()
+    .on('log', function (data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function (done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
+});
+
+gulp.task('build', ['sass', 'sass-ionic']);
