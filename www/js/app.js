9f022929cd1adb04c3b3569b2b8318cb16571baf
s7mile <haeru29@gmail.com>
2016-10-04 22:23:24 +0900

    타이틀예제 빌드까지

--- www/js/app.js
+++ www/js/app.js
@@ -2421,7 +2421,11 @@ app.controller('scrapViewModalController', function ($scope, $ionicModal) {
 
   console.log('세이브모달 컨트롤러');
 
-  $scope.$parent.closeModal()
+  $scope.$parent.closeModal();
+
+  angular.element("#add_title").click(function(){
+  	$(this).after("<div class='title-exam'><strong>이런 제목은 어떤가요?</strong><ol><li>자동차 컨트롤 개선사례<a href='javascript:;'>사용하기</a></li><li>자동차 컨트롤 개선 방법론<a href='javascript:;'>사용하기</a></li><li>자동차 컨트롤 연구분야<a href='javascript:;'>사용하기</a></li></ol></div>");
+  });
 
 });
 
